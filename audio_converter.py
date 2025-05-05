e93e34cddd7c62a383512f38586d0bbfa5a26cbb
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2025-05-05 21:16:27 +0300

    project initialization and started creating the GUI class

---
+++ audio_converter.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+
+class Audio_Convert(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("PDF to Audio Converter")
+        self.geometry("720x480")
